c0ee157a3b9a01ac69d46f8ec7841e3fc1b56f9e
QuadDepo <ryan@qdds.nl>
2021-05-13 21:37:12 +0200

    Add Routes

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,13 @@
 import mongoose from 'mongoose';
+import * as filmRoutes from './routes/films.route';
+import * as planetRoutes from './routes/planets.route';
 import express from 'express';
 const MONGO_URL = "mongodb://127.0.0.1:27017/geeksforgeeks";
 const PORT = 9000;
 mongoose.connect(MONGO_URL, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
+APP.use('/films', filmRoutes.getFilms);
+APP.use('/film/:id', filmRoutes.getFilmById);
+
+APP.use('/planets/', planetRoutes.getPlanets);
+APP.use('/planet/:id', planetRoutes.getPlanetById);
 });
