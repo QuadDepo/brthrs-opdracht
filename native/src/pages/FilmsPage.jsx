7800aa458de5374146b5fe5fe995388346ff64d0
QuadDepo <ryan@qdds.nl>
2021-05-17 23:26:19 +0200

    Add Film Page content

--- native/src/pages/FilmsPage.jsx
+++ native/src/pages/FilmsPage.jsx
@@ -1,6 +1,56 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useState, useEffect, useContext } from "react";
+import { View, FlatList, Text, TouchableOpacity } from "react-native";
+import { SearchBar } from "react-native-elements";
+import { NavigationContext } from "@react-navigation/native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function FilmsPage() {
-  return <View style={{ flex: 1, backgroundColor: "#1C2222" }}></View>;
+// Get Film Consts
+import { GET_FILMS } from "../const";
+// Import Film context
+import { useFilmContext } from "../context/Films";
+
+
+export default function FilmPage() {
+  const [search, setSearch] = useState("");
+  // Get Navigation Context
+  const { navigate } = useContext(NavigationContext);
+  // Get state and dispatch from Film Context
+  const { state, dispatch } = useFilmContext();
+
+  useEffect(() => {
+    dispatch({ type: GET_FILMS, payload: { title: search } });
+  }, [search]);
+
+  useEffect(() => {
+    dispatch({ type: GET_FILMS, payload: { title: "" } });
+  }, []);
+
+  const flatListItem = ({ item }) => (
+    <TouchableOpacity onPress={() => navigate("FilmDetail", {id: _id})}>
+      <Text>{item.title}</Text>
+    </TouchableOpacity>
+  );
+
+  return (
+    <View style={{ backgroundColor: "#1C2222", flex: 1 }}>
+      <SafeAreaView edges={["right", "top", "left"]} style={{ flex: 1 }}>
+        <SearchBar
+          containerStyle={{
+            borderBottomColor: "transparent",
+            borderTopColor: "transparent",
+            backgroundColor: "transparent",
+          }}
+          value={search}
+          onChangeText={(text) => setSearch(text)}
+          onClear={() => setSearch("")}
+          placeholder="A new ... Job / Hope."
+        />
+        <FlatList
+          data={state.films?.data?.results}
+          keyExtractor={(film) => film._id}
+          renderItem={flatListItem}
+        />
+      </SafeAreaView>
+    </View>
+  );
 }
