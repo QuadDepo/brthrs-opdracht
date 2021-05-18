bc6530ebab431c40fed91b338465b39d29ba74df
QuadDepo <ryan@qdds.nl>
2021-05-18 18:55:49 +0200

    Add Sorting to Film Details

--- native/src/pages/FilmDetail.jsx
+++ native/src/pages/FilmDetail.jsx
@@ -10,6 +10,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 import Pagination from "../components/Pagination";
 import Filter from "../components/Filter";
+import Sorting from "../components/Sorting";
 // Get Film Consts
 import { GET_SINGLE_FILM, GET_CHARACTERS_BY_FILM } from "../const";
 // Import Film context
@@ -29,6 +30,12 @@ const styles = StyleSheet.create({
     color: "#fff",
   },
 
+  subTitle: {
+    marginTop: 30,
+    fontSize: 24,
+    color: "#fff",
+  },
+
   opening_crawl: {
     width: "100%",
     fontSize: 20,
@@ -44,12 +51,24 @@ export default function FilmDetail({ route, navigation }) {
 
   const [page, setPage] = useState(1);
   const [filters, setFilters] = useState();
+  const [sorting, setSorting] = useState();
 
-  const filter = {
-    filterKey: "gender",
+  const sortingData = [
+    {
+      name: "Age",
+      sortingKey: "age",
+    },
+    {
+      name: "Height",
+      sortingKey: "height",
+    },
+  ];
+
+  const filterData = {
     name: "Gender",
+    filterKey: "gender",
     data: ["n/a", "male", "female"],
-    onChange: (filter) => setFilters({...filters, filter})
+    onChange: (filter) => setFilters({ ...filters, filter }),
   };
 
   useEffect(() => {
@@ -59,14 +78,21 @@ export default function FilmDetail({ route, navigation }) {
   }, []);
 
   useEffect(() => {
+    // Set movie title as page Title
     navigation.setOptions({ title: state.film.data?.title });
   }, [state.film.data.title]);
 
   useEffect(() => {
-    console.log(filters);
-    console.log({...filters});
-    dispatch({ type: GET_CHARACTERS_BY_FILM, payload: { id, page, ...filters } });
-  }, [page, filters]);
+    dispatch({
+      type: GET_CHARACTERS_BY_FILM,
+      payload: { id, page, ...filters, sorting },
+    });
+  }, [page, sorting, filters]);
+
+  useEffect(() => {
+    // If filters change move back to page 1
+    setPage(1);
+  }, [filters]);
 
   const flatListItem = ({ item }) => (
     <TouchableOpacity>
@@ -75,13 +101,19 @@ export default function FilmDetail({ route, navigation }) {
   );
 
   return (
-    <ScrollView style={{ backgroundColor: "#1C2222", flex: 1 }}>
+    <ScrollView>
       <SafeAreaView style={styles.page}>
         <Text style={styles.title}>{state.film.data?.title}</Text>
         <Text style={styles.opening_crawl}>
           {state.film.data?.opening_crawl}
         </Text>
-        <Filter {...filter} />
+        <Text style={styles.subTitle}>Filters</Text>
+        <Filter {...filterData} />
+        <Text style={styles.subTitle}>Sort</Text>
+        <Sorting
+          sortingData={sortingData}
+          onChange={(sort) => setSorting(sort)}
+        />
         <Pagination
           {...state.characters.data}
           onChange={(newPage) => setPage(newPage)}
